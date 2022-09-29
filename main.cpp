f13b38df997941fb144017b4a0987b4f5e2d159c
Lencho398 <112590909+Rafa0928@users.noreply.github.com>
2022-09-29 00:18:17 +0000

    lab2

--- main.cpp
+++ main.cpp
@@ -1,36 +1,39 @@
 #include <iostream>
 using namespace std;
-int main() {
-  const double RATE2 = 3.10;
-  const double RATE6 = 4.20;
-  const double RATE10 = 6.70;
-  const double RATE20 = 8.80;
-  double weight, distance, dRate, price;
+int main()
+{
+        const double RATE2 = 3.10;
+        const double RATE6 = 5.20;
+        const double RATE10 = 6.70;
+        const double RATE20 = 8.80;
+        double weight, distance, dRate, price;
 
-  cout << "Enter the package weight and distance\n";
-  cin >> weight >> distance;
-  //? Input Validation
-  if ((weight < 0) && (weight > 20)) {
-    cout << "The package weight must be a positive number and less than 20.\n";
-    exit(0);
-  }
+        cout << "Enter the package weight and distance\n";
+        cin >> weight >> distance;
+        //? Input Validation
+        if ((weight < 0) && (weight > 20))
+        {
+                cout << "The package weight must be a positive number and less than 20.\n";
+                exit(0);
+        }
 
-  if (weight < 2)
-    dRate = RATE2;
-  else if (weight < 6)
-    dRate = RATE6;
-  else if (weight < 10)
-    dRate = RATE10;
-  else if (weight <= 20)
-    dRate = RATE20;
-  else {
-    cout << "The weight must be less than 20\n";
-    exit(0);
-  }
+        if (weight < 2)
+                dRate = RATE2;
+        else if (weight < 6)
+                dRate = RATE6;
+        else if (weight < 10)
+                dRate = RATE10;
+        else if (weight <= 20)
+                dRate = RATE20;
+        else
+        {
+                cout << "The weight must be less than 20\n";
+                exit(0);
+        }
 
-  if (distance < 500)
-    price = dRate;
-  else
-    price = (distance / 500.0) * dRate;
-  cout << "The shipping price for package is " << price << endl;
+        if (distance < 500)
+                price = dRate;
+        else
+                price = (distance / 500.0) * dRate;
+        cout << "The shipping price for package is " << price << endl;
 }
